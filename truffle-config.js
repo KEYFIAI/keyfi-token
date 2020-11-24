ab923420098960ba931106775b7ef293af4f1a19
Carlos Bruguera <cbruguera@gmail.com>
2020-11-24 15:03:06 +0700

    updated gas prices

--- truffle-config.js
+++ truffle-config.js
@@ -37,7 +37,7 @@ module.exports = {
       provider: () => RopstenProvider,
       network_id: 3,
       gas: 5000000,
-      gasPrice: 40000000000, // 5 gwei
+      gasPrice: 110000000000, // 110 gwei
       // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
@@ -47,7 +47,7 @@ module.exports = {
       network_id: 1,
       //from: addresses[0],
       gas: 5000000,
-      gasPrice: 8000000000, // 8 gwei
+      gasPrice: 110000000000, // 110 gwei
       //skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     }
     // Useful for private networks
