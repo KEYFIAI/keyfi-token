e36b10076b887ae6e3f092b3ef7455978448c41e
Carlos Bruguera <cbruguera@gmail.com>
2020-11-25 11:28:12 +0700

    migration parameter changes

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,7 +1,7 @@
 let KeyfiToken = artifacts.require('KeyfiToken.sol')
 let account = '0xFdb6ac6750690D9c46d038f2536eC995D9d3D6d8'
-let minter = '0x856bcd7F92436F749Af98DE309785A42F747B79a'
-let mintingAllowedAfter = 0
+let minter =  '0x0000000000000000000000000000000000000000'
+let mintingAllowedAfter = 1669161600
 
 module.exports = async deployer => {
   
--- migrations/3_deploy_rewardpool.js
+++ migrations/3_deploy_rewardpool.js
@@ -1,6 +1,6 @@
 let RewardPool = artifacts.require('RewardPool.sol')
-let rewardPerBlock = "700000000000000000" // 0.7 tokens per block
-let tokenAddress = '0xe3C64a8c516B393e8805Bcc8713189DC3B00e153'
+let rewardPerBlock = "636500000000000000" // 0.6365 tokens per block
+let tokenAddress = '0x7C63008B7ed8534215Ff942826499BAcDd18D729'
 let whitelist = '0xa40fC5a9232868B0b681B9C34F3081be32368ad3'
 
 module.exports = async deployer => {
