099969d3e44568d0e0d9374ea83481631999fd75
Carlos Bruguera <cbruguera@gmail.com>
2020-12-12 15:53:07 +0700

    added audit report document

--- migrations/7_deploy_all_ropsten.js
+++ migrations/7_deploy_all_ropsten.js
@@ -5,17 +5,17 @@ let TreasuryVester = artifacts.require('TreasuryVester.sol')
 let whitelist = '0xa40fC5a9232868B0b681B9C34F3081be32368ad3'
 let account = '0xFdb6ac6750690D9c46d038f2536eC995D9d3D6d8'
 let minter = '0xFdb6ac6750690D9c46d038f2536eC995D9d3D6d8'
-let mintingAllowedAfter = 0
+let mintingAllowedAfter = 1607427900
 
 let rewardPerBlock = web3.utils.toWei('1') //"1000000000000000000" // 1 token per block
 //let launchDate = 1607322000
-let launchDate = 9999999999
+let launchDate = 1607427000
 
 let amount = web3.utils.toWei('958333')
 let begin = launchDate 
 let cliff = begin
 //let end = begin + (year * 2)
-let end = begin + 1800
+let end = 1607428800//begin + 1800
 
 let teamWallet = '0x856bcd7F92436F749Af98DE309785A42F747B79a'
 let treasuryWallet = '0xe83E4e869dD693349B0c8F2755EbD46169CA5b75'
